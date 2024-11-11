d9b9b926e1c1840e38bb1517b3e312d5c23df39b
CarlosBuendiaVazquez <l21530240@cancun.tecnm.mx>
2024-11-11 12:47:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -38,8 +38,7 @@
     <a href="practica1.php"> Practica 1</a>
     <h2>Practica 2</h2>
     <a href="practica2.php"> Practica 2</a>
-    <h2>Practica 3</h2>
-    <a href="practica3.php"> Practica 3</a>
+    
     
 
     
