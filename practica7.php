287f50d2c97051c60febe73765f605fc2721952f
CarlosBuendiaVazquez <l21530240@cancun.tecnm.mx>
2024-11-15 10:26:31 -0500

    Create practica7.php

---
+++ practica7.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+ <title>Práctica 7 </title>
+</head>
+<body>
+
+<h1>Imagen de creación para la base de datos</h1>
+<img src="https://i.postimg.cc/fTdpK0HX/Imagen-practica-7.png">
+
+</body>
+</html>
