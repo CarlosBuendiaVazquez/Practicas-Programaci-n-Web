1854afabe6e46b71213975797a90e82d03bbd012
CarlosBuendiaVazquez <l21530240@cancun.tecnm.mx>
2024-11-18 11:08:40 -0500

    Create config6.php

---
+++ config6.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+ <title>Mi primer Script en php </title>
+</head>
+<body>
+
+<?php
+$servidor = "miservidor1234.database.windows.net"; 
+$usuario = "adminUser";                      
+$password = "web3333.";             
+$bd = "myDatabase";                          
+?>
+
+
+</body>
+</html>
