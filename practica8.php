0121ace602812eecb06269539e4813e0dcd7562b
CarlosBuendiaVazquez <l21530240@cancun.tecnm.mx>
2024-11-22 09:57:10 -0500

    Create practica8.php

---
+++ practica8.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+ <title>Práctica 8 </title>
+</head>
+<body>
+
+<h1>Imagen de eliminación para una base de datos</h1>
+<img src="https://i.postimg.cc/gksdfP1W/Whats-App-Image-2024-11-22-at-9-48-57-AM.jpg">
+
+</body>
+</html>
