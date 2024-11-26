0aa6d932a993d832c6841f7a03c81b62a829f011
CarlosBuendiaVazquez <l21530240@cancun.tecnm.mx>
2024-11-26 11:18:47 -0500

    Create practica10.php

---
+++ practica10.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+ <title>Práctica 10 </title>
+</head>
+<body>
+
+<h1>Imagen para el listado de base de datos</h1>
+<img src="https://i.postimg.cc/8kZvKs1c/Captura-de-pantalla-2024-11-26-100201.png">
+
+</body>
+</html>
